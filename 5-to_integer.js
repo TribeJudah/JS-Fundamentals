3d473515b1f1a2c6cf7a746ee72c5d9956106813
Terry Jumah <Jehovahjay@gmail.com>
2025-11-15 22:22:10 +0000

    Add created file 5-to_integer.js

---
+++ 5-to_integer.js
@@ -0,0 +1,8 @@
+const args = process.argv.slice(2);
+const toInteger = Math.floor(Number(args[0]));
+
+if (Number.isInterger(toInteger)) {
+    console.log(`My number: ${toInteger}`);
+} else {
+    console.log("Not a number");
+}
