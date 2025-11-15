5f3afb1b680bf0dbb0eecdf7c492bbcf80e9895b
Terry Jumah <Jehovahjay@gmail.com>
2025-11-15 22:46:33 +0000

    Add current file 7-multi_c.js

---
+++ 7-multi_c.js
@@ -0,0 +1,10 @@
+const args = process.argv.slice(2);
+const toInteger = Math.floor(Number(args[0]));
+
+if (Number.isInteger(toInteger)) {
+    for (let i = 0; i< toInteger; i++) {
+        console.log("C is fun");
+    }
+} else {
+    console.log("Missing numner of occurrences");
+}
