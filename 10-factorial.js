a05e0c2e6629bc11090ff2b4a5697840a6e0ad4e
Terry Jumah <Jehovahjay@gmail.com>
2025-11-15 23:22:12 +0000

    Add this script that computes and prints a factorial

---
+++ 10-factorial.js
@@ -0,0 +1,16 @@
+const args = process.argv.slice(2);
+
+function factorial(num) {
+    const toNumber = Number(num);
+
+    if (Number.isNan(toNumber)) return 1;
+
+    if(toNumber === 1) return 1;
+
+    if(toNumber >= 2) {
+        return toNumber * factorial(toNumber - 1);
+    }
+}
+
+const result = factorial(args[0]);
+console.log(result);
